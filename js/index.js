5ab645522ec6106548905ee44334b2d4e077f62c
Barrett Crusse <cvillebear@gmail.com>
2023-09-05 15:08:31 -0400

    Remove onload listener

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-window.onload = function () {
-	let img = document.getElementById("image");
-	if (window.location.href.includes("github.io")) {
-		img.src = "https://picsum.photos/id/200/200/300";
-		img.alt = "image displayed because ON github pages";
-	}
-};
+let img = document.getElementById("image");
+if (window.location.href.includes("github.io")) {
+	img.src = "https://picsum.photos/id/200/200/300";
+	img.alt = "image displayed because ON github pages";
+}
