dc47b79717d7eadf9d7e89823fbd36b07bdb5aa5
Barrett Crusse <cvillebear@gmail.com>
2023-09-05 15:09:27 -0400

    Change href to host

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let img = document.getElementById("image");
-if (window.location.href.includes("github.io")) {
+if (window.location.host.includes("github.io")) {
 	img.src = "https://picsum.photos/id/200/200/300";
 	img.alt = "image displayed because ON github pages";
 }
