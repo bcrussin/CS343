afebf0b74290e32c90636722ad068daac7c3a98f
Barrett Crusse <cvillebear@gmail.com>
2023-09-05 14:46:46 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+	let img = document.getElementById("image");
+	if (window.location.href.includes("github.io")) {
+		img.src = "https://picsum.photos/id/200/200/300";
+		img.alt = "image displayed because ON github pages";
+	}
+};
